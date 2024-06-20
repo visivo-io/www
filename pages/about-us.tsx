102ef70146c485aeb6a7113a414754d2b42de66f
Tim <timothy@overly.me>
2024-06-19 21:15:42 -0400

    Fix page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -7,16 +7,16 @@ export default function ContactPage() {
     <Page title="Who are we?">
       <PrivacyPolicyContainer>
         <RichText>
-          <p>We are a small team that is passionate about building great tools for analytics. 
-             We have deep experience in software development and business analytics.</p>
+          <p>We are a small team that is passionate about building great tools for analytics.
+            We have deep experience in software development and business analytics.</p>
           <br />
           <br />
           <p>We want to solve a problem that we have personally experienced.</p>
           <strong>Business intelligence visualizations are too brittle.</strong>
           <br />
           <br />
-          <p>We may not be the right fit for your company, but reach out and we'll be happy to make tooling recommendations.</p>
-          <strong>We're the best solution for technical teams or companies with in-house analytics. </strong>
+          <p>We may not be the right fit for your company, but reach out and we&#39;ll be happy to make tooling recommendations.</p>
+          <strong>We&#39;re the best solution for technical teams or companies with in-house analytics. </strong>
           <br />
           <br />
           <p>We want to provide clear value.</p>
