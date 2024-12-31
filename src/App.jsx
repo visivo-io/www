c00e810b622c755e809a9bba27ff1f755a1b3b02
jpjsonic <jpjsonic@gmail.com>
2024-12-31 13:55:00 -0500

    Added missing mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,21 @@ function App() {
           <img src="/images/logo.png" className="mr-3 h-6 sm:h-9" alt="Visivo Logo" />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Visivo</span>
         </Navbar.Brand>
+        
+        <div className="flex md:order-2">
+          <DarkThemeToggle className="mr-2" />
+          <a href="https://app.visivo.io">
+            <Button color="primary">App</Button>
+          </a>
+          <Navbar.Toggle />
+        </div>
+        
         <Navbar.Collapse>
           <Navbar.Link href="/examples">Examples</Navbar.Link>
           <Navbar.Link href="/pricing">Pricing</Navbar.Link>
           <Navbar.Link href="https://docs.visivo.io">Docs</Navbar.Link>
         </Navbar.Collapse>
-        <div className="flex">
-          <DarkThemeToggle className="mr-2" />
-          <a href="https://app.visivo.io">
-            <Button color="primary">App</Button>
-          </a>
-        </div>
-      </Navbar >
+      </Navbar>
 
       <main className="w-full flex-col items-center justify-center gap-2 dark:bg-gray-800">
         <Outlet />
