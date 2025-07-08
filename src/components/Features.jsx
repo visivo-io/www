fba54502288c1575240895d84b7d54baf92286c4
jpjsonic <jpjsonic@gmail.com>
2025-07-08 12:08:31 -0400

    Re ordered feature sections in order of how compelling I think they are

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -46,25 +46,25 @@ const Section = ({ imageUrl, title, description, details, reversed }) => {
 export default function UxFeatures() {
   return (
     <div>
-      <div className="pt-5 dark:bg-gray-900">
+      <div className="mt-5 py-20 dark:bg-gray-900">
         <div className="mx-auto max-w-7xl">
-          <div className="mt-10 text-center text-5xl font-bold dark:text-gray-300">Craft an unparalleled user experience</div>
+          <div className="text-center text-5xl font-bold dark:text-gray-300">Simple to Deploy & Collaborate</div>
           <Section
-            imageUrl="/images/chart-types.webp"
-            title="Layout & configurations options"
-            description="Unblock Creativity"
-            details="With over 20+ trace types and thousands of configuration options, you can build the perfect dashboard for your stakeholders." />
+            imageUrl="/images/pull-request.webp"
+            title="Keep your HEAD clear & commit to git"
+            description="Version Control"
+            details="Collaborate with your team, track changes, and maintain your dashboards with the same tools you use for the rest of your code." />
           <Section
             reversed={true}
-            imageUrl="/gifs/webp/interactivity.webp"
-            title="Interactivity without slow load times"
-            description="Interactive"
-            details="Unlike traditional BI tools, Visivo does not need to re-fetch data every time a user interacts with your dashboard." />
+            imageUrl="/images/git-workflow.webp"
+            title="Deployments that mirror your workflow"
+            description="CI/CD"
+            details="Super charge your development cycle with seamless local development and a CI/CD pipeline that deploys projects to all of your environments." />
           <Section
-            imageUrl="/images/laptop-iphone-asset.webp"
-            title="Meet your stakeholders where they are"
-            description="Mobile Optimized"
-            details="Deliver your insights to your team on the go and in the office." />
+            imageUrl="/images/continuous-testing.webp"
+            title="Goodbye broken dashboards"
+            description="Testable"
+            details="Test new charts, tables and dashboards before they go live in production.  Ensure your tests are passing before data refresh.  Receive alerts when your dashboards are broken and fix problems before your stakeholders are impacted." />
         </div>
       </div>
       <div className="mt-5 bg-gray-100 py-20 dark:bg-gray-800">
@@ -89,27 +89,28 @@ export default function UxFeatures() {
             details="Jinja2 templating allows you to dynamically render your dashboards based on environment variables, conditional logic, and loops." />
         </div>
       </div>
-      <div className="mt-5 py-20 dark:bg-gray-900">
+      <div className="pt-5 dark:bg-gray-900">
         <div className="mx-auto max-w-7xl">
-          <div className="text-center text-5xl font-bold dark:text-gray-300">Simple to Deploy & Collaborate</div>
+          <div className="mt-10 text-center text-5xl font-bold dark:text-gray-300">Craft an unparalleled user experience</div>
           <Section
-            imageUrl="/images/pull-request.webp"
-            title="Keep your HEAD clear & commit to git"
-            description="Version Control"
-            details="Collaborate with your team, track changes, and maintain your dashboards with the same tools you use for the rest of your code." />
+            imageUrl="/images/chart-types.webp"
+            title="Layout & configurations options"
+            description="Unblock Creativity"
+            details="With over 20+ trace types and thousands of configuration options, you can build the perfect dashboard for your stakeholders." />
           <Section
             reversed={true}
-            imageUrl="/images/git-workflow.webp"
-            title="Deployments that mirror your workflow"
-            description="CI/CD"
-            details="Super charge your development cycle with seamless local development and a CI/CD pipeline that deploys projects to all of your environments." />
+            imageUrl="/gifs/webp/interactivity.webp"
+            title="Interactivity without slow load times"
+            description="Interactive"
+            details="Unlike traditional BI tools, Visivo does not need to re-fetch data every time a user interacts with your dashboard." />
           <Section
-            imageUrl="/images/continuous-testing.webp"
-            title="Goodbye broken dashboards"
-            description="Testable"
-            details="Test new charts, tables and dashboards before they go live in production.  Ensure your tests are passing before data refresh.  Receive alerts when your dashboards are broken and fix problems before your stakeholders are impacted." />
+            imageUrl="/images/laptop-iphone-asset.webp"
+            title="Meet your stakeholders where they are"
+            description="Mobile Optimized"
+            details="Deliver your insights to your team on the go and in the office." />
         </div>
       </div>
+      
     </div>
   );
 }
